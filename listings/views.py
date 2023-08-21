909f094d60b355b8de6148386c9f3f6f432cdd07
Aqib <hafizaqibakhtar@nitb.gov.pk>
2023-08-21 12:09:33 +0500

    Changes

--- listings/views.py
+++ listings/views.py
@@ -21,7 +21,7 @@ def listing_retrieve(request, id):
 def listing_create(request):
     form = ListingForm()
     if request.method == "POST":
-        form = ListingForm(request.POST)
+        form = ListingForm(request.POST, request.Files)
         if form.is_valid():
             form.save()
             return redirect("/")
@@ -36,7 +36,7 @@ def listing_update(request, pk):
     form = ListingForm(instance=listing)
 
     if request.method == "POST":
-        form = ListingForm(request.POST, instance=listing)
+        form = ListingForm(request.POST, instance=listing, files=request.FILES)
         if form.is_valid():
             form.save()
             return redirect("/")
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
 from listings.views import (listing_list, listing_retrieve, listing_create, listing_update, listing_delete)
 
 urlpatterns = [
@@ -11,3 +12,6 @@ urlpatterns = [
     path('listings/<pk>/delete', listing_delete),
     path('add-listing/', listing_create),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
